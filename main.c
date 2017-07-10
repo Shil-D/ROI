e188cec71ed23af52c7e7ee15f476b4ea68d60c7
Shil.D <Shil.D@yandex.ru>
2017-07-10 18:27:38 +0300

    add hello string

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    char helloStr[]=
+            "______ _____ _____ \n"
+            "| ___ \\  _  |_   _|\n"
+            "| |_/ / | | | | |  \n"
+            "|    /| | | | | |  \n"
+            "| |\\ \\\\ \\_/ /_| |_ \n"
+            "\\_| \\_|\\___/ \\___/ \n"
+                              ;
+    printf("%s\n\n", helloStr);
+    printf("Enter your database filename:\n:");
     return 0;
 }
