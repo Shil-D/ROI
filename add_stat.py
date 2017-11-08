a4df9533f93f0764df7344cca47e75c14d00bb7d
Шиляев Денис Васильевич <SBT-Shilyaev-DV@sigma.sbrf.ru>
2017-11-08 15:10:56 +0300

    stats

--- add_stat.py
+++ add_stat.py
@@ -9,34 +9,40 @@ import pandas as pd
 import os
 import sys
 import argparse
+import logging as log
 
 BASE_COLS = ['Amount']
-def add_to_portfolio(file, base):
+
+def add_to_portfolio(folder, base):
     if os.path.exists(base):
+        log.info('output file ' + base + ' already exists')
         df = pd.read_excel(base)
     else:
+        log.info("output file ' + base + ' doesn't exists. Creating new one.")
         df = pd.DataFrame(columns=BASE_COLS)
         
-    htmlf = pd.read_html(file)
-    
-    acc_df = htmlf[1]
-    acc_df.columns = ['Name', 'Start', 'End', 'Diff']
-    start_date = pd.to_datetime(acc_df['Start'][0][-10:], dayfirst = True)
-    start_amnt = float(acc_df['Start'][7].replace(',', '.').replace(' ', ''))
-    
-    df.loc[start_date] = {
-        'Amount' : start_amnt
-    }
-    
-    df.sort_index(inplace = True)
-    ex_writer = pd.ExcelWriter(base, engine='xlsxwriter',
-                               datetime_format = 'dd.mm.yyyy',
-                               date_format = 'dd.mm.yyyy')
-    df.to_excel(ex_writer)
+    for file in os.listdir(folder):
+        log.info('folud file ' + file)
+        htmlf = pd.read_html(os.path.join(folder,file))
+        
+        acc_df = htmlf[1]
+        acc_df.columns = ['Name', 'Start', 'End', 'Diff']
+        start_date = pd.to_datetime(acc_df['Start'][0][-10:], dayfirst = True)
+        start_amnt = float(acc_df['Start'][7].replace(',', '.').replace(' ', ''))
+        
+        df.loc[start_date] = {
+            'Amount' : start_amnt
+        }
+        
+        df.sort_index(inplace = True)
+        ex_writer = pd.ExcelWriter(base, engine='xlsxwriter',
+                                   datetime_format = 'dd.mm.yyyy',
+                                   date_format = 'dd.mm.yyyy')
+        df.to_excel(ex_writer)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', type=str)
     parser.add_argument('base', type = str)
+    parser.add_argument('folder', type=str,help='Input html folder')
     args = parser.parse_args(sys.argv[1:])
-    add_to_portfolio(args.filename, args.base)
+    add_to_portfolio(args.folder, args.base)
---
+++ stats.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 15:58:17 2017
+
+@author: shil.d
+"""
+
+import pandas as pd
+import os
+import numpy as np
+import argparse
+import sys
+import matplotlib.pyplot as plt
+
+def max_dd(ser):
+    max2here = ser.expanding().max()
+    dd2here = ser - max2here
+    return dd2here.min()
+
+def max_ddd(ser):
+    ddd = ser.copy()
+    ddd[0] = 0
+    max2here = ser.expanding().max()
+    dd2here = max2here - ser
+    for i in range(1,len(dd2here != 0)):
+        if dd2here[i-1] == 0:
+            ddd[i] = 0
+        else:
+            ddd[i] = ddd[i-1] + 1
+    return ddd.max()
+
+def main(base_file, risk_free_file):
+    df = pd.read_excel(base_file)
+    risk_free = pd.read_excel(risk_free_file)
+    amnt = df.Amount
+    risk_free = risk_free.reindex(index = df.index, method='ffill', copy=False)
+    df['Daily_Profits'] = amnt.diff()
+    df['Daily_Returns'] = amnt.pct_change()
+    daily_rets = df['Daily_Returns']
+    cum_rets = daily_rets.cumsum() + 1
+    risk_free_rates = risk_free['RiskFreeRate'] / 252
+    df['Excess_Returns'] = daily_rets - risk_free_rates
+    exc_rets = df['Excess_Returns']
+    exc_cum_rets = exc_rets.cumsum() + 1
+    sharpe = np.sqrt(252)*exc_rets.mean()/exc_rets.std()
+    mdd = max_dd(cum_rets)
+    mddd = max_ddd(cum_rets)
+    props = dict(boxstyle= 'round', facecolor='wheat', alpha=0.5)
+    textstr = 'Sharpe = %.2f\nMaxDD = %d\nMaxDDD = %d' % (sharpe, mdd, mddd)
+    fig, ax = plt.subplots(1)
+    cum_rets.plot(ax = ax)
+    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+    plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('base', type = str)
+    parser.add_argument('risk_free', type=str)
+    args = parser.parse_args(sys.argv[1:])
+    main(args.base, args.risk_free)
